60ca1f701dc5159c19e79cb2d1da712b72db3511
Eduardo Vargas <eduardo704@gmail.com>
2020-02-21 16:24:58 -0300

    initial commit -scss working

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let name;
+</script>
+
+<style type="text/scss" global>
+  $color: blue;
+  $primary: purple;
+  @import "./styles/global.scss";
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: $color;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+
+    .hello {
+      font-size: 45px;
+    }
+  }
+</style>
+
+<main>
+  <h1 class="hello">Hello {name}!</h1>
+  teste
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
