a25a8fefe47e2bfac0586e632ceab721ef5acd48
Eduardo Vargas <eduardo704@gmail.com>
2020-02-21 18:29:58 -0300

    better css and componetization

---
+++ src/features/Home.svelte
@@ -0,0 +1,20 @@
+<style type="text/scss">
+  @import "../styles/global.scss";
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+  .full {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<main class="full">
+  <div class="text-light content">
+    <h1>Brazilian dev, I know mostly angular and web stuff</h1>
+  </div>
+</main>
